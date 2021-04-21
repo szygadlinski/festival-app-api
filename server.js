a791d46e7b1c6893e1cde0029995c847569a55dd
Szymon Zygadliński <szygadlinski@gmail.com>
2021-04-21 02:05:52 +0200

    Add simple database

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = [
+  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+  { id: 3, author: 'Olivia Lee', text: 'This company is worth every coin!' },
+  { id: 4, author: 'Barry Ross', text: 'They really know how to make you happy.' },
+  { id: 5, author: 'Diane Carter', text: 'This company is worth every coin!' },
+  { id: 6, author: 'Ralph Smith', text: 'They really know how to make you happy.' },
+];
+
 app.listen(7000, () => {
   console.log('Server is running on port 7000: http://localhost:7000');
 });
