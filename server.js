2cbef901699d4fde68616590b0fe75ffbe8ed7fa
Szymon Zygadliński <szygadlinski@gmail.com>
2021-04-23 01:03:38 +0200

    Add endpoint to app & modify port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
@@ -11,6 +12,7 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors(corsOptions));
@@ -18,10 +20,14 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: '404 - Page not found.' });
 });
 
-app.listen(7000, () => {
+app.listen(process.env.PORT || 7000, () => {
   console.log('Server is running on port 7000: http://localhost:7000');
 });
